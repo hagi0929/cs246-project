df0456deb6875298adca871cf9e17b6993c83ed5
j257choi <j257choi@uwaterloo.ca>
2023-11-26 20:21:49 -0500

    error solved and prepared to start

--- src/controller/game.cc
+++ src/controller/game.cc
@@ -2,20 +2,28 @@
 #include <iostream>
 using namespace std;
 
-Game::Game(GameBoard *board, Display *display): board{board}, display{display} {
+Game::Game(GameBoard *board, Display *display) : board{board}, display{display}
+{
     cout << "Game ctor is called" << endl;
 }
 
-Game::~Game() {
+Game::~Game()
+{
     // delete currentGame;
     cout << "Game dtor is called" << endl;
 }
 
-void Game::play() {
+void Game::play()
+{
     cout << "Game::play() is called" << endl;
 }
 
-bool Game::isValidCmd() const {
+bool Game::isValidCmd() const
+{
     cout << "Game::isValidCmd() is called" << endl;
     return true;
+}
+
+void Game::activate()
+{
 }
